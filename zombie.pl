35b57a840b87cf59bc793ad9945c96f8a1a7b638
Christoph Berg <christoph.berg@credativ.de>
2013-01-24 17:25:01 +0100

    Add a test script for SIGCHLD/EINTR.
    Thanks to Arnd Hannemann for help debugging the problem.

---
+++ zombie.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+# This script forks twice, and the middle process exits immediately. The third
+# process will then gets pid 1 as parent, and exit later. At that point, pid 1
+# receives a SIGCHLD signal. The first process keeps running so we can actually
+# observe this effect.
+
+if (fork == 0) {
+	if (fork == 0) {
+		sleep 1;
+		exit 1;
+	} else {
+		exit 2;
+	}
+} else {
+	sleep 2;
+	exit 3;
+}
